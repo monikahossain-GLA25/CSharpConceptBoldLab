983d6e9b540f0a9b0dcc8b80000f1225176e0dde
Monika Hossain <monikahossain25@gmail.com>
2026-09-21 00:33:30 +0600

    Add generic Box practice

---
+++ Generic/Box.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpConceptBoldLab.Generic
+{
+    public class Box <T>
+    {
+        public T content { get; set; }
+
+        public string Log()
+        {
+            return $"Box contains: {content}";
+        }
+
+        
+    }
+}
---
+++ Generic/Program.cs
@@ -0,0 +1,21 @@
+﻿using CSharpConceptBoldLab.Generic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpConceptBoldLab.Generic
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<int> objBox = new Box<int>();
+            objBox.content = 42;
+            Console.WriteLine(objBox.Log());
+
+            Box<string> strBox = new Box<string>();
+            strBox.content = "Monik Hossain`";
+            Console.WriteLine(strBox.Log());
+        }
+    }
+}
